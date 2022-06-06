e282db4b10b9b36a1ae8edc6905374f38849007d
DiegoAGtz <diegogtz1207@gmail.com>
2022-06-06 13:55:43 -0500

    Fix ListadoPacientes -> PacientesProvider moved to AdministrarPacientes

--- src/App.jsx
+++ src/App.jsx
@@ -12,32 +12,26 @@ import EditarPerfil from "./paginas/EditarPerfil";
 import CambiarPassword from "./paginas/CambiarPassword";
 
 import { AuthProvider } from "./context/AuthProvider";
-import { PacientesProvider } from "./context/PacientesProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <PacientesProvider>
-          <Routes>
-            <Route path="/" element={<AuthLayout />}>
-              <Route index element={<Login />} />
-              <Route path="registrar" element={<Registrar />} />
-              <Route path="olvide-password" element={<OlvidePassword />} />
-              <Route
-                path="olvide-password/:token"
-                element={<NuevoPassword />}
-              />
-              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-            </Route>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+          </Route>
 
-            <Route path="/admin" element={<RutaProtegida />}>
-              <Route index element={<AdministrarPacientes />} />
-              <Route path="perfil" element={<EditarPerfil />} />
-              <Route path="cambiar-password" element={<CambiarPassword />} />
-            </Route>
-          </Routes>
-        </PacientesProvider>
+          <Route path="/admin" element={<RutaProtegida />}>
+            <Route index element={<AdministrarPacientes />} />
+            <Route path="perfil" element={<EditarPerfil />} />
+            <Route path="cambiar-password" element={<CambiarPassword />} />
+          </Route>
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   );
